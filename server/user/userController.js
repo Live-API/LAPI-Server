203001acd3cf59ef5dca9ac5bfde1d5c960df2c4
Brett Beekley <brettbeekley@gmail.com>
2017-07-26 11:37:37 -0700

    Added user model using mongoose

---
+++ server/user/userController.js
@@ -0,0 +1,47 @@
+const User = require('./userModel');
+const bcrypt = require('bcryptjs');
+
+const userController = {};
+
+userController.getAllUsers = (next) => {
+  User.find({}, next);
+};
+
+/**
+* createUser - create a new User model and then save the user to the database.
+*/
+
+userController.createUser = async (req, res, next) => {
+  // Create a user
+  try {
+    const user = new User(req.body);
+    // bcrypt being done in mongoose middleware in userModel
+    res.locals.userid = (await user.save())._id;
+    next();
+  } catch (err) {
+    // Send an error message
+    res.send(err);
+  }
+};
+
+/**
+* verifyUser - Obtain username and password from the request body, locate
+* the appropriate user in the database, and then authenticate the submitted password
+* against the password stored in the database.
+*/
+userController.verifyUser = (req, res, next) => {
+  // Query db for username in req body
+  User.findOne({ username: req.body.username }, (err, user) => {
+    // If it exists continue
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+      res.locals.userid = user._id;
+      next(); 
+    }
+    // Else send an error message
+    else {
+      res.send('Incorrect username or password');
+    }
+  });
+};
+
+module.exports = userController;
