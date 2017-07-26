0b95c9acd3ba0d6f72eefb159c35e3cb9d2701bc
Brett Beekley <brettbeekley@gmail.com>
2017-07-26 11:06:49 -0700

    Added unintesting config route

--- server/server.js
+++ server/server.js
@@ -5,8 +5,12 @@ const PORT = 4000;
 
 app.get('/', (req, res) => {
     res.send('Home page');
-})
+});
+
+app.get('/config', (req, res) => {
+  res.send('config page');
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening on Port ${PORT}`);
-})
+});
