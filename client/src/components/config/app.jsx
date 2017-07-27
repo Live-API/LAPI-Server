fcc569b0fa54c5639656ab7e3c83adc37d1a0405
Brett Beekley <brettbeekley@gmail.com>
2017-07-27 11:56:49 -0700

    createAdmin runs on form submission, prep for POST

--- client/src/components/config/app.jsx
+++ client/src/components/config/app.jsx
@@ -5,18 +5,27 @@ import CreateUserDialog from './createUser.jsx';
 
 class App extends Component {
   
+  // POSTS to server to create the initial admin user
+  createAdmin(data) {
+//    const route = '/config/admin';
+//    const request = new XMLHttpRequest();
+//    request.open('POST', route, true);
+//    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+//    request.send(data);
+    console.log('A user was submitted:', data);
+  }
+  
   render() {
-    
+    // So grid elements are centered on entire page
     const gridStyle = { height: '100%' }
-    
     return (
       <Grid centered verticalAlign='middle' columns={2} style={gridStyle}>
         <Grid.Column>
-          <CreateUserDialog />
+          <CreateUserDialog type='administrator' submission={this.createAdmin}/>
         </Grid.Column>
       </Grid>
     )
-  }9
+  }
 }
 
 export default App;
