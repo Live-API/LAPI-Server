9cdeeaedfa7127849c07132b9ee1526648aca896
pennwu1 <phw.dental@gmail.com>
2017-08-01 10:56:49 -0700

    update crawlerController logic

--- server/crawler/crawler.js
+++ server/crawler/crawler.js
@@ -1,25 +1,55 @@
 const NodeCrawler = require('crawler');
 const Crawler = require('./crawlerModel.js');
-// Create a new instance of the node crawler
+
+/* Crawler calls 'callback' function expression, scraping website (c.queue)
+for Cheerio selectors. Document is created using data array, and saved to the Crawler model. */
+
+// *** represents items that will be provided by the config file
+
+let data = [];
+
 const c = new NodeCrawler({
     maxConnections: 10,
     callback: (error, res, done) => {
         if (error) console.log(error);
         else {
             const $ = res.$;
-            // Select the DOM elements to extract with Cheerio
-            // Not working
-            let data = [];
-            $('.cardDetails > ul:first-child').each((index, element) => {
-                data.push($(element).text());
-            });
-            console.log(data);
-            // Crawler.save();
-            // save each item to the Mongoose database
-            // console.log("this is the anchor element", a);
+            async function extractData() {
+                try {
+                    // *** Selectors for DOM elements
+                    await $('.cardDetails').each((index, element) => {
+                        data.push($(element).text());
+                    });
+                    // Support for Pagination
+                    let href = $('[aria-label="Next page"]').attr('href');
+                    // If href exists, scrape next page
+                    if (href) c.queue('https:' + href);
+                    // At end of pagination, add/update document
+                    else {
+                        let scrapedData = new Crawler({
+                            // *** Endpoint Name
+                            endpoint: "config",
+                            data: JSON.stringify(data)
+                        })
+                        // Replace existing data property if the document exists
+                        const cachedData = (await Crawler.find({endpoint: "config"}));
+                        if (cachedData.length > 0) {
+                            await Crawler.update({endpoint: 'config'}, { $set: { data: JSON.stringify(data) }});
+                        // Create a new document if scraping for the first time
+                        } else {
+                            await scrapedData.save();
+                        }
+                    }
+                    console.log(await Crawler.find({}));
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+            extractData();
         };
         done();
     }
 });
 
+// *** url from config file is passed to queue
 c.queue('https://www.trulia.com/CA/San_Francisco/');
