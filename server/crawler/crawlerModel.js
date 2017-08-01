85c07bc89240e947258fa55e480f83a1ccd06bdb
Brett Beekley <brettbeekley@gmail.com>
2017-08-01 14:35:02 -0700

    Global promises library used for mongoose in crawlerModel

--- server/crawler/crawlerModel.js
+++ server/crawler/crawlerModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.connect('localhost:27017');
 
+mongoose.Promise = global.Promise;
+
 const crawlerSchema = new Schema({
     endpoint: {type: String, required: true}, // config file
     scrape_date: {type: Date, default: Date.now(), required: true},
