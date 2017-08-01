ba431de39901a5dfae748ba99bc09686a514194c
Brett Beekley <brettbeekley@gmail.com>
2017-08-01 14:50:56 -0700

    Updated to non-deprecated connection methods http://mongoosejs.com/docs/connections.html#use-mongo-client

--- server/crawler/crawlerModel.js
+++ server/crawler/crawlerModel.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-mongoose.connect('localhost:27017');
+mongoose.connect('mongodb://localhost:27017', {
+  useMongoClient: true,
+});
 
 mongoose.Promise = global.Promise;
 
+const INTERVAL = 60;
+
 const crawlerSchema = new Schema({
-    endpoint: {type: String, required: true}, // config file
-    scrape_date: {type: Date, default: Date.now(), required: true},
-    data: {type: Object, required: true} // crawler
+  endpoint: {type: String, required: true}, // config file
+  scrape_date: {type: Date, default: Date.now(), required: true},
+  data: {type: Object, required: true}, // crawler
+  interval: {type: Number, default: INTERVAL, required: true}, // Amount of time between scrapes, in seconds
 });
 
 module.exports = mongoose.model("Crawler", crawlerSchema);
--- server/user/userModel.js
+++ server/user/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-mongoose.connect('localhost:27017');
+mongoose.connect('mongodb://localhost:27017', {
+  useMongoClient: true,
+});
 
 mongoose.Promise = global.Promise;
 
