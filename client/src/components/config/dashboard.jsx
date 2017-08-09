56673e5adf09e29bfbb39431dcd0459ec0e409b0
Brett Beekley <brettbeekley@gmail.com>
2017-08-09 10:58:05 -0700

    Dashboard as class

--- client/src/components/config/dashboard.jsx
+++ client/src/components/config/dashboard.jsx
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Card, Input, Divider, Button, Form, Icon } from 'semantic-ui-react'
 
-const Dashboard = (props) => {
-  return  (
-    <Card fluid color='red'>
-      <Card.Content>
-        <Card.Header>
-          Dashboard!
-        </Card.Header>
-      </Card.Content>
-      <Card.Content description='Pizza!' />
-      <Card.Content extra>
-        {props.message}
-      </Card.Content>
-    </Card>
-  );
+class Dashboard extends Component {
+
+  render() {
+    return  (
+      <Card fluid color='red'>
+        <Card.Content>
+          <Card.Header>
+            Dashboard!
+          </Card.Header>
+        </Card.Content>
+        <Card.Content description='Pizza!' />
+        <Card.Content extra>
+          {this.props.message}
+        </Card.Content>
+      </Card>
+    );
+  }
 }
 
 export default Dashboard;
