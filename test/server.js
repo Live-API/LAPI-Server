8598afb9451397ca4247a3f259fee695a6474318
Brett Beekley <brettbeekley@gmail.com>
2017-08-21 14:09:30 -0700

    Tests for initial admin

--- test/server.js
+++ test/server.js
@@ -1,17 +1,49 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../server/server.js');
-var should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server/server.js');
+const should = chai.should();
+const User = require('./../server/user/userModel');
 
 chai.use(chaiHttp);
 
-describe('Initial Admin Setup', function() {
-  it('should show a login/register page on /config GET', done => {
+/*
+ *  This set of tests tests for API functionality,
+ *  not front-end display and behavior
+ */
+
+describe('User Creation (\"Authentication\" routes in server.js)', function() {
+  // Can we load the config page?
+  it('should successfully send html page on /config GET', done => {
     chai.request(server)
     .get('/config')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.html;
+      done();
+    });
+  });
+  // Can we get bundled files?
+  it('should send bundled js for the /config page', done => {
+    chai.request(server)
+    .get('/static/bundles/config.bundle.js')
     .end(function(err, res){
       res.should.have.status(200);
       done();
     });
   });
+  // Can we create the initial admin?
+  it('should create admin if no users in db, without requiring cookies', done => {
+    chai.request(server)
+    .post('/config/admin')
+    .field('username', 'test' + Math.floor(100 * Math.random()))
+    .field('password', 'test')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      
+      done();
+    });
+  });
+  // Can we generate an invite?
+  // it('should ');
 });
